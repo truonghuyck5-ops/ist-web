bdf1763428b7bf3ff310fdf476df83f1ebc4b095
HuyIST <truonghuyck5@gmail.com>
2026-05-06 16:38:27 +0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="min-h-screen bg-black text-white flex items-center justify-center">
+    <div class="text-center">
+      <h1 class="text-6xl font-bold text-orange-500 mb-4">
+        IST WEBSITE
+      </h1>
+
+      <p class="text-gray-300 text-xl">
+        Thiết kế - In ấn - Quảng cáo
+      </p>
+    </div>
+  </div>
+`
