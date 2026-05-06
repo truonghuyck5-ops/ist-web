c4bb8b40a75cc87a3cd77ac49118948bed14a40a
HuyIST <truonghuyck5@gmail.com>
2026-05-06 16:47:24 +0700

    test

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ document.querySelector('#app').innerHTML = `
   <div class="min-h-screen bg-black text-white flex items-center justify-center">
     <div class="text-center">
       <h1 class="text-6xl font-bold text-orange-500 mb-4">
-        IST WEBSITE
+        IST WEBSITE 2
       </h1>
 
       <p class="text-gray-300 text-xl">
