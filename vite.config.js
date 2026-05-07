2eaef9c85482f54672c8d62c79d3f0bd660dd024
HuyIST <truonghuyck5@gmail.com>
2026-05-07 10:27:54 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        temNhan: resolve(__dirname, 'tem-nhan.html'),
+      },
+    },
+  },
 })
